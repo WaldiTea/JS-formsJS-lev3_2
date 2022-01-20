06a33d3b675db2be5d6918af34e9d47cdd5048bd
SuperCoder <supercoder@MacBook-Air-von-SuperCoder.local>
2022-01-20 08:40:47 +0100

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,5 @@ function changeBackground() {
   let red = document.getElementById('red').value;
   let green = document.getElementById('green').value;
   let blue = document.getElementById('blue').value;
-  let color = 'rgb(' + red + ',' + green + ',' + blue + ')';
-  document.body.style.backgroundColor = color;
+  document.body.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
 }
